4bcfcee8a2f17ae6e05b7fd0fad73d8df5126d42
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-05-29 07:34:14 +0000

    test Get API

---
+++ Book/app/Http/Controllers/apiResMaiAnh.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class apiResMaiAnh extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //hien thi user
+        $user = User::all();
+        $data = json_decode($user, true);
+
+        return response()->json($data);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ Book/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/store', 'App\Http\Controllers\apiResMaiAnh@store');
