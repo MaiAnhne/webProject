276477617c91d3fd9ad6a025ea59f4ec8d19e0e5
MaiAnhne <151895735+MaiAnhne@users.noreply.github.com>
2025-06-24 02:13:01 +0000

    xincamon

--- Book/routes/web.php
+++ Book/routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\OrderController;
 
 Route::middleware(['auth'])->group(function() {
     Route::resource('books', BookController::class);
+});
+
+Route::middleware(['auth'])->group(function() {
+    Route::resource('orders', OrderController::class)->only(['index','create','store','show']);
 });
 
 Route::get('/', function () {
